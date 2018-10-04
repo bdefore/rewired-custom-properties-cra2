9672bffd2fc860db552b5dd11cf4656960e93fde
Buck DeFore <bdefore@gmail.com>
2018-10-04 15:28:03 +0000

    update without the direct import to verify postcss rewire is not applying

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
           This should be green, set by local custom property
         </div>
         <div className={styles.test3}>
-          This should be green, set by custom property from variables.json
+          This should be green, set by custom property from variables.css
         </div>
         <div>
           This should be the default black
